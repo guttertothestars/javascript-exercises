e135830560acc2f1d2b66b0c11be4fcea60b7733
guttertothestars <wwilkson@gmail.com>
2023-03-14 14:07:06 -0400

    complete excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...allTheNums) {
+   let results = [];
+   for(let i = 0; i < arr.length; i++) {
+    if (allTheNums.includes(arr[i])) { 
+      continue; 
+    } else {
+      results.push(arr[i])
+   }
+  }
+   return results;
 };
 
 // Do not edit below this line
