86f9521e5b4eae76b6e6eed184642850677f89d5
guttertothestars <wwilkson@gmail.com>
2023-03-14 13:03:36 -0400

    complete excercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let output = ''
+  for(let i = 0; i < num; i++) {
+    output += string;
+  }
+  
+  if (num < 0) {
+    output = "ERROR";
+  }
 
+  return output;
 };
 
 // Do not edit below this line
