25cec5df4c886313b3d248ece7c2161e0f068516
guttertothestars <wwilkson@gmail.com>
2023-03-15 13:48:33 -0400

    Coplete excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
-
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+const sumAll = function(num1, num2) {
+  //SETUP: initialize sum, determine start and end, throw errors for edge cases
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  if (!Number.isInteger(num1) || !Number.isInteger(num2) ) return 'ERROR';
+    
+    let sum = 0;
+    let start = 0;
+    let end = 0;
+  
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    } else {
+        start = num1;
+        end = num2;
+    }
+  
+  //I can't believe javascript doesn't have a simple range function
+    let range = [];
+    for (let i = start; i <= end; i++) {
+      range.push(i);
+    }
+  
+  //loop using range array to add to sum
+    for(let i = 0; i < range.length; i++) {
+      sum += range[i];
+    }
+    return sum;
+  };
+  
+  // Do not edit below this line
+  module.exports = sumAll;
+  
